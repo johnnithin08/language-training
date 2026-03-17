2d20741f1faa4bf9777bdd409e55e64d4bc64105
johnnithin08 <johnnithin08@gmail.com>
2026-03-17 21:56:22 +0000

    feat: landing page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout() {
   return (
     <>
       <Stack screenOptions={{ headerShown: false }} />
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </>
   );
 }
